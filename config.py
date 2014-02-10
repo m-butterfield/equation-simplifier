3897b823c373fa527b3095269934bf3195578379
Matt Butterfield <mbutterfield@onshift.com>
2014-02-09 22:00:12 -0500

    start database stuff

---
+++ config.py
@@ -0,0 +1,7 @@
+from local_config import *
+
+# define these in your local_config.py file
+DEBUG = local_config.DEBUG
+SECRET_KEY = local_config.SECRET_KEY
+DATABASE_URI = local_config.DATABASE_URI
+
--- machine/__init__.py
+++ machine/__init__.py
@@ -1,4 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
 import machine.views
+from machine.database import db_session
