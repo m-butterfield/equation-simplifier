84062d0d9ead48e7413cfc429f773dfab4b14261
Matt Butterfield <mbutterfield@onshift.com>
2014-02-15 12:50:18 -0500

    parse and return simplified expression

--- machine/views.py
+++ machine/views.py
@@ -1,5 +1,9 @@
 from machine import app
+from sympy.parsing.sympy_parser import parse_expr,\
+    standard_transformations, implicit_multiplication_application
 from flask import render_template, jsonify, request
+
+transformations = standard_transformations + (implicit_multiplication_application,)
 
 @app.route('/')
 def index():
@@ -7,13 +11,16 @@ def index():
 
 @app.route('/simplify')
 def simplify():
-    expression = request.args.get('expression', '', type=str)
-
-    print expression
-
+    expression = request.args.get('expression', '', type=str).replace('^', '**')
     if not expression:
         return jsonify(error=True)
+    result = None
+    try:
+        result = str(parse_expr(expression,
+                                transformations=transformations)).replace('**', '^')
 
-
-
-    return jsonify(simplified_expr='result: ' + expression)
+    except:
+        return jsonify(error=True)
+    if not result:
+        return jsonify(error=True)
+    return jsonify(simplified_expr=result)
